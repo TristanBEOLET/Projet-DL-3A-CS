0b58b57426d9100924437005c6465e50b1c83d0d
Inès <63157521+Screzprojet26@users.noreply.github.com>
2023-03-25 15:48:59 +0100

    Add files via upload

---
+++ gan_model.py
@@ -0,0 +1,165 @@
+import logging
+import pickle
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import random_split
+from torchvision import transforms
+
+import data_utils
+import metrics
+import utils
+
+torch.manual_seed(0)
+
+
+class VNet(nn.Module):
+    def __init__(self):
+        super(VNet, self).__init__()
+        # Define the layers of the V-Net model
+        self.batch_norm0 = nn.BatchNorm3d(1)
+        self.conv1 = nn.Conv3d(1, 32, kernel_size=3, stride=2)
+        self.conv2 = nn.Conv3d(32, 64, kernel_size=3, stride=2)
+        self.conv3 = nn.Conv3d(64, 128, kernel_size=3, stride=1)
+        self.deconv4 = nn.ConvTranspose3d(128, 64, kernel_size=3, stride=1)
+        self.deconv5 = nn.ConvTranspose3d(64, 32, kernel_size=3, stride=2)
+        self.deconv6 = nn.ConvTranspose3d(
+            32, 1, kernel_size=3, stride=2, output_padding=(1, 1, 1)
+        )
+
+    def forward(self, x):
+        # Define the forward pass of the V-Net model
+        x = self.batch_norm0(x)
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.deconv4(x)
+        x = F.relu(x)
+        x = self.deconv5(x)
+        x = F.relu(x)
+        x = self.deconv6(x)
+        return x
+
+class Modeldiscriminator(nn.Module):
+    def __init__(self, input_channels=1):
+        super(Modeldiscriminator, self).__init__()
+        self.conv1 = nn.Conv3d(input_channels, 64, kernel_size=4, stride=2, padding=1)
+        self.conv2 = nn.Conv3d(64, 128, kernel_size=4, stride=2, padding=1)
+        self.conv3 = nn.Conv3d(128, 256, kernel_size=4, stride=2, padding=1)
+        self.conv4 = nn.Conv3d(256, 512, kernel_size=4, stride=2, padding=1)
+        self.conv5 = nn.Conv3d(512, 1, kernel_size=1, stride=1, padding=0)
+        self.leakyrelu = nn.LeakyReLU(0.2, inplace=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.leakyrelu(x)
+        x = self.conv2(x)
+        x = self.leakyrelu(x)
+        x = self.conv3(x)
+        x = self.leakyrelu(x)
+        x = self.conv4(x)
+        x = self.leakyrelu(x)
+        x = self.conv5(x)
+        x = self.sigmoid(x)
+        return x
+
+
+def main():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s - %(filename)s:%(funcName)s - %(message)s",
+        datefmt="%H:%M:%S",
+    )
+    logging.info("Loading data")
+
+    # Saving start time to use as a timestamp
+    time_stamp = time.strftime("%Y%m%d-%H%M%S")
+
+    # Constants
+    hyperparameters_gn = utils.Hyperparameters(
+        num_epochs=500,
+        batch_size=64,
+        training_split_ratio=0.8,
+        lr=0.001,
+        weight_decay=1e-5,
+        treshold=0.5,
+    )
+    hyperparameters_gn.save(time_stamp=time_stamp)
+
+    hyperparameters_dc = utils.Hyperparameters(
+        num_epochs=500,
+        batch_size=64,
+        training_split_ratio=0.8,
+        lr=0.001,
+        weight_decay=1e-5,
+        treshold=0.5,
+    )
+    hyperparameters_dc.save(time_stamp=time_stamp)
+
+    logging.debug(f"Learning rate generator: {hyperparameters_gn.lr}")
+    logging.debug(f"weight decay generator: {hyperparameters_gn.weight_decay}")
+    logging.debug(f"Prediction threshold generator: {hyperparameters_gn.treshold}")
+    logging.debug(f"Batch size generator: {hyperparameters_gn.batch_size}")
+    logging.debug(f"Training split ratio generator : {hyperparameters_gn.training_split_ratio}")
+
+    logging.debug(f"Learning rate discriminator: {hyperparameters_dc.lr}")
+    logging.debug(f"weight decay discriminator: {hyperparameters_dc.weight_decay}")
+    logging.debug(f"Prediction threshold discriminator: {hyperparameters_dc.treshold}")
+    logging.debug(f"Batch size discriminator: {hyperparameters_dc.batch_size}")
+    logging.debug(f"Training split ratio discriminator : {hyperparameters_dc.training_split_ratio}")
+
+    # Data
+    transform = transforms.Compose(
+        [
+            data_utils.Resize3D((128, 128, 20)),
+        ]
+    )
+
+    dataset = data_utils.MSD_Brain_Tumor("Task01_BrainTumour", transform=transform)
+    nb_train = int(hyperparameters_gn.training_split_ratio * len(dataset))
+    train, test = random_split(dataset, [nb_train, len(dataset) - nb_train])
+
+    train_dataloader = DataLoader(
+        train, batch_size=hyperparameters_gn.batch_size, shuffle=True
+    )
+    test_dataloader = DataLoader(
+        test, batch_size=hyperparameters_gn.batch_size, shuffle=False
+    )
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    generator = VNet().float().to(device)
+    discriminator = Modeldiscriminator().float.to(device)
+
+    (
+        train_dice_scores,
+        train_mse_scores,
+        train_discri_scores,
+        test_dice_scores,
+        test_mse_scores,
+    ) = utils.train_model(
+        generator, 
+        discriminator,
+        hyperparameters_gene=hyperparameters_gn,
+        hyperparameters_discri=hyperparameters_dc,
+        train_dataloader=train_dataloader,
+        test_dataloader=test_dataloader,
+        device=device,
+        time_stamp=time_stamp,
+    )
+
+    metrics.plot_training_metrics(time_stamp)
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils_gan.py
@@ -0,0 +1,183 @@
+import logging
+import pickle
+from typing import Tuple
+
+import metrics
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import utils
+
+
+class Hyperparameters:
+    def __init__(
+        self, num_epochs, batch_size, training_split_ratio, lr, weight_decay, treshold
+    ) -> None:
+        self.num_epochs = num_epochs
+        self.batch_size = batch_size
+        self.training_split_ratio = training_split_ratio
+        self.lr = lr
+        self.weight_decay = weight_decay
+        self.treshold = treshold
+
+    def save(self, time_stamp):
+        pickle.dump(
+            self,
+            open(
+                f"checkpoints/{time_stamp}_hyperpameters.pkl",
+                "wb",
+            ),
+        )
+
+
+def save_training_data(
+    
+    train_dice_scores: list,
+    train_mse_scores: list,
+    train_discri_scores : list,
+    test_dice_scores: list,
+    test_mse_scores: list,
+    time_stamp: list,
+) -> None:
+    """Save all training losses and scores to checkpoint folder.
+
+    Args:
+        train_dice_scores (list): _description_
+        train_mse_scores (list): _description_
+        test_dice_scores (list): _description_
+        test_mse_scores (list): _description_
+        time_stamp (list): _description_
+    """
+    np.savez(
+        f"checkpoints/{time_stamp}_training_run.npz",
+        train_dice_scores=train_dice_scores,
+        train_mse_scores=train_mse_scores,
+        train_discri_scores = train_discri_scores,
+        test_dice_scores=test_dice_scores,
+        test_mse_scores=test_mse_scores,
+    )
+
+
+def test_model(
+    model, test_dataloader, criterion, treshold, device
+) -> Tuple[float, float]:
+    """Test model on testing dataloader."""
+    model.eval()
+    test_dice_loss_sum = 0
+    test_mse_loss_sum = 0
+
+    with torch.no_grad():
+        for data in test_dataloader:
+            inputs = data[0].float()[:, None, :, :, :].to(device)
+            labels = (data[1] != 0.0).float()[:, None, :, :, :].to(device)
+            outputs = model(inputs)
+            dice_loss = 1 - metrics.dice(outputs > treshold, labels)
+            mse_loss = criterion(outputs, labels)
+            loss = dice_loss + mse_loss
+            test_dice_loss_sum += dice_loss.item()
+            test_mse_loss_sum += mse_loss.item()
+
+    return 1 - (test_dice_loss_sum / len(test_dataloader)), test_mse_loss_sum / len(
+        test_dataloader
+    )
+
+
+def train_model(
+    generator, discriminator, hyperparameters_gn, hyperparameters_discri, train_dataloader, test_dataloader, device, time_stamp
+):
+    """Train model, saves the model with the best validation dice score, and final model."""
+    criterion_gn = nn.MSELoss(reduction="mean")
+    criterion_dc = nn.BCELoss()
+    optimizer_gn = optim.Adam(
+        generator.parameters(),
+        lr=hyperparameters_gn.lr,
+        weight_decay=hyperparameters_gn.weight_decay,
+    )
+
+    optimizer_discri = optim.Adam(
+        generator.parameters(),
+        lr=hyperparameters_discri.lr,
+        weight_decay=hyperparameters_discri.weight_decay,
+    )
+
+    # Keeping the losses in memory to plot training evolution
+    train_dice_losses = []
+    train_mse_losses = []
+
+    train_dice_scores = []
+    train_mse_scores = []
+
+    test_dice_scores = []
+    test_mse_scores = []
+
+    train_discri_losses = []
+    train_discri_scores = []
+
+    logging.info(f"Starting training for {hyperparameters_gn.num_epochs} epochs")
+    for epoch in range(hyperparameters_gn.num_epochs):
+        generator.train()
+        discriminator.train()
+        dice_loss_sum = 0
+        mse_loss_sum = 0
+        discri_loss_sum = 0
+        for i, data in enumerate(train_dataloader):
+            inputs = data[0].float()[:, None, :, :, :].to(device)
+            labels = (data[1] != 0.0).float()[:, None, :, :, :].to(device)
+            optimizer_gn.zero_grad()
+            optimizer_discri.zero_grad()
+            outputs = generator(inputs)
+            dice_loss = 1 - metrics.dice(outputs > hyperparameters_gn.treshold, labels)
+            mse_loss = criterion_gn(outputs, labels)
+            loss_gene = dice_loss + mse_loss
+            loss_gene.backward()
+            discri_real_loss = criterion_dc(discriminator(labels), torch.ones(labels.size(0), 1).to(device))
+            discri_fake_loss = criterion_dc(discriminator(outputs.detach()), torch.zeros(outputs.size(0), 1).to(device))
+            loss_discri = discri_real_loss + discri_fake_loss
+            loss_discri.backward()
+            optimizer_discri.step()
+            dice_loss_sum += dice_loss.item()
+            mse_loss_sum += mse_loss.item()
+            discri_loss_sum += loss_discri.item()
+            optimizer_gn.step()
+            optimizer_discri.step()
+        train_dice_losses.append(dice_loss_sum)
+        train_mse_losses.append(mse_loss_sum)
+        train_discri_losses.append(discri_loss_sum)
+
+        dice_score = 1 - (dice_loss_sum / len(train_dataloader))
+        mse_score = mse_loss_sum / len(train_dataloader)
+        discri_score = discri_loss_sum/ / len(train_dataloader)
+        train_dice_scores.append(dice_score)
+        train_mse_scores.append(mse_score)
+        train_discri_scores.append(discri_score)
+
+        test_dice_score, test_mse_score = utils.test_model(
+            model=generator,
+            test_dataloader=test_dataloader,
+            criterion=criterion_gn,
+            treshold=hyperparameters_gn.treshold,
+            device=device,
+        )
+
+        logging.info(
+            f"Epoch {epoch+1}/{hyperparameters_gn.num_epochs}, Train dice: {dice_score:.4f}, Train MSE : {mse_score:.5f}, Train Discri : {discri_score:.5f}, Test dice: {test_dice_score:.4f}, Test MSE : {test_mse_score:.5f}"
+        )
+
+        if test_dice_score > max(test_dice_scores, default=0):
+            logging.info(f"Saving model for test dice score : {test_dice_score:.4f}")
+            torch.save(generator, f"checkpoints/{time_stamp}_best_model.pt")
+
+        test_dice_scores.append(test_dice_score)
+        test_mse_scores.append(test_mse_score)
+
+    torch.save(generator, f"checkpoints/{time_stamp}_final_model.pt")
+    save_training_data(
+        train_dice_scores=train_dice_scores,
+        train_mse_scores=train_mse_scores,
+        train_discri_scores=train_discri_scores,
+        test_dice_scores=test_dice_scores,
+        test_mse_scores=test_mse_scores,
+        time_stamp=time_stamp,
+    )
+    return train_dice_scores, train_mse_scores, train_discri_scores, test_dice_scores, test_mse_scores
